bd33643650fd4a995848f5d3876b53f04ecee6a0
clement-tourbez <clement.tourbez@umons.ac.be>
2019-02-18 19:42:09 +0100

    day3

--- data.R
+++ data.R
@@ -2,9 +2,9 @@ SciViews::R
 
 
 (hip <- as_dataframe(tibble(
-  stalk = rep(c(1, 2,3,4,5),2),
-  height = c(26.8,1.5,1.3,0.5,2.3,28.5,0.3,1.5,1.3,2.4 ),
-  day = rep(c(1,2),each = 5)
+  stalk = rep(c(1, 2,3,4,5),3),
+  height = c(26.8,1.5,1.3,0.5,2.3,28.5,1.5,1.3,0.3,2.4,31.2,1.6,1.4,0.7,2.4 ),
+  day = rep(c(1,2,3),each = 5)
 )))
 
 hip$stalk <- as.factor(hip$stalk)
